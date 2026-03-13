0b732cf4606186f51dc1c82ba5e43860b114caf5
Saml <slopez152@students.avc.edu>
2026-03-12 19:36:04 -0700

    quiz

--- main.cpp
+++ main.cpp
@@ -8,25 +8,22 @@ int main()
     double a, b, c;
 
 
-    double root1, root2;
-
 
     cin >> a >> b >> c;
 
 
     double discriminant = b * b - 4 * a * c;
 
-    root1 = (-b + sqrt(discriminant)) / (2 * a);
+    double root1 = (-b + sqrt(discriminant)) / (2 * a);
 
 
-    root2 = (-b - sqrt(discriminant)) / (2 * a);
+    double root2 = (-b - sqrt(discriminant)) / (2 * a);
     
 
-    if (root2 > root1) {
-        swap(root1, root2);
-    }
+    if (root2 > root1) swap(root1, root2);
+
 
     cout << setprecision(2) << fixed;
-    cout << "Roots: " << root1 << " " << root2 << endl;
+    cout << root1 << " " << root2 << endl;
         return 0;
 }
